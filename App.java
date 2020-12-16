835a25bffccddce0f571ef2d84525515e8eac94e
JohnCircuit <john.veerling4@gmail.com>
2020-12-16 15:32:45 +0000

    Speaking to the player if they want to play or not.

--- App.java
+++ App.java
@@ -1,5 +1,38 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+      intro();
+
+      public static void intro()
+      {
+        Scanner scanner = new Scanner(System.in);
+      System.out.println("It looks like you want to be dealt in, is that true?");
+      String start = scanner.nextLine();
+      String answer1 = "yes";
+      String answer2 = "Yes";
+      while (start != answer1 || start != answer2)
+      {
+        if (start != answer1 || start != answer2)
+        {
+          System.out.println("Sorry I didn't quite get that.");
+          start = scanner.nextLine();
+          return start;
+        }
+
+        }
+        System.out.println("Alright");
+      
+
+
+
+
+
+
+
+
+
+
     }
 }
