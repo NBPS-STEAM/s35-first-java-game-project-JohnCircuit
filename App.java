46ece2bbaff335e9b38e2e816688dd13a1b3696b
JohnCircuit <john.veerling4@gmail.com>
2020-12-16 15:43:26 +0000

    It can detect your answer but it can't use logic to tell if it is right or wrong. It is only saying your answer is wrong.

--- App.java
+++ App.java
@@ -2,37 +2,39 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-
+      System.out.println("It looks like you want to be dealt in, is that true?");
       intro();
-
+    }
       public static void intro()
       {
         Scanner scanner = new Scanner(System.in);
-      System.out.println("It looks like you want to be dealt in, is that true?");
+      
       String start = scanner.nextLine();
       String answer1 = "yes";
       String answer2 = "Yes";
-      while (start != answer1 || start != answer2)
+      while (!start.equals(answer1) || !start.equals(answer2))
       {
-        if (start != answer1 || start != answer2)
+        if (!start.equals(answer1) || !start.equals(answer2))
         {
-          System.out.println("Sorry I didn't quite get that.");
           start = scanner.nextLine();
-          return start;
+          System.out.println("Sorry I didn't quite get that.");
+          //start = scanner.nextLine();
+          
         }
 
         }
         System.out.println("Alright");
       
-
-
-
-
-
-
-
-
-
-
-    }
+      }
 }
+
+
+
+
+
+
+
+
+
+    
+
