90e8dedbd2372c5a22878a69a952f6156d8e03cb
JohnCircuit <john.veerling4@gmail.com>
2020-12-18 20:04:27 +0000

    Progress Save

--- App.java
+++ App.java
@@ -2,8 +2,10 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
+      //Scanner scanner = new Scanner(System.in);
       System.out.println("It looks like you want to be dealt in, is that true?");
       intro();
+      drawOrHold();
     }
       public static void intro()
       {
@@ -11,13 +13,13 @@ public class App {
       
       String start = scanner.nextLine();
       
-      while (!start.equals("yes") || !start.equals("Yes"))
+      while (start.equals("yes") || start.equals("Yes"))
       {
         if (!start.equals("yes") || !start.equals("Yes"))
         {
-          start = scanner.nextLine();
-          System.out.println("Sorry I didn't quite get that.");
           //start = scanner.nextLine();
+          System.out.println("Sorry I didn't quite get that.");
+          start = scanner.nextLine();
           
         }
         if (start.equals("yes") || start.equals("Yes"))
@@ -25,7 +27,7 @@ public class App {
           System.out.println("Alright lets start!");
           break;
         }
-        }
+      } 
         
       
       }
@@ -38,6 +40,21 @@ public class App {
     }
     public static void drawOrHold()
     {
+      Scanner scanner = new Scanner(System.in);
+      System.out.println("How many times would you like to draw?");
+      int draw = scanner.nextInt();
+
+      if (draw <= 0)
+      {
+        //break;
+      }
+      else if (draw >= 1)
+      {
+        for(int i = 1; i <= draw; i++)
+    	{
+		    System.out.println("Hello Karel");
+    	}
+      }
       //if draw, ask how many to draw
       //if hold, stop game
 
