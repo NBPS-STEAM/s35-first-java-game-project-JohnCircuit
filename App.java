b08283a8c090d87b1656fb00ca2c30eed1dc3233
JohnCircuit <john.veerling4@gmail.com>
2020-12-20 21:22:35 +0000

    AHHHH it will do all the randomization, but I can't move the numbers into the next function yet.

--- App.java
+++ App.java
@@ -1,12 +1,17 @@
 import java.util.Scanner;
 import java.util.Random;
 public class App {
+
+  static int hand;
+  static int sum;
     public static void main(String[] args) {
       //Scanner scanner = new Scanner(System.in);
       System.out.println("It looks like you want to be dealt in, is that true?");
       intro();
       drawOrHold();
     }
+
+
       public static void intro()
       {
         Scanner scanner = new Scanner(System.in);
@@ -33,22 +38,27 @@ public class App {
       } 
         
       
-      }
-    public static void addScore()
+    }
+    public static int addScore()
     { 
       //Get randomNumber
       //randomNumber += userScore
       //return userScore
       Random card = new Random();
+      App hand = new App();
       
       int score;
       score = 1+card.nextInt(11);
       System.out.println(score);
-      
+      App.hand = score;
+      return App.hand;
       
       
     }
-    public static void drawOrHold()
+
+
+
+    public static int drawOrHold()
     {
       Scanner scanner = new Scanner(System.in);
       System.out.println("How many times would you like to draw?");
@@ -62,9 +72,12 @@ public class App {
         for(int i = 1; i <= draw; i++)
     	{
         addScore();
-		    
+        int sum = sum + App.hand;
+		    //System.out.println(App.hand);
     	}
-      
+      //System.out.println(App.hand);
+      return App.hand();
+      System.out.println(sum);
       }
       //if draw, ask how many to draw
       //if hold, stop game
