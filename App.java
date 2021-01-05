3c79960d75b3583a16f0b8d0af711363a6c6837c
JohnCircuit <john.veerling4@gmail.com>
2021-01-05 21:21:28 +0000

    Comments -____-

--- App.java
+++ App.java
@@ -1,12 +1,13 @@
 import java.util.Scanner;
 import java.util.Random;
+//Imports that the code will use to call on.
 public class App {
-
+//The main class.
   static int hand;
   static int sum;
     public static void main(String[] args) {
-      //Scanner scanner = new Scanner(System.in);
       
+      //Down below it is asking you a question, and depending on the result it will either loop or let you continue.
       System.out.println("It looks like you want to be dealt in, is that true?");
 
         Scanner scanner = new Scanner(System.in);
@@ -31,11 +32,11 @@ public class App {
           break;
         } 
       }
-
+      //Down below is the function that will actually play the game.
       drawOrHold();
       
       System.out.println("Now that you know what the game is like, how many times would you like to play again?"); 
-
+      //Here it is asking you how many times you would like to play after you already played your first game.
       int rerun = scanner.nextInt();
       int i = 0;
       while (i < rerun)
@@ -50,6 +51,7 @@ public class App {
 
     public static void drawOrHold()
     {
+      //It starts off by asking you how many cards you would like to draw, from there it will tell you if you won or not. Depending on what cards you get you will get a particular prompt.
       Scanner scanner = new Scanner(System.in);
       System.out.println("How many times would you like to draw?");
       int draw = scanner.nextInt();
@@ -67,8 +69,9 @@ public class App {
         System.out.println(score);
         
         hand = hand + score;
-        
+        //This is how it will generate a random number and add it to your hand.
     	}
+        //Down below is all the esponses to if you win or lose, if you are over 21 or under 20 then you lose, but if you are within the range you win!
         System.out.println(hand);
         if (hand < 20)
         {
